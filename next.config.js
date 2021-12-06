eee342d12f225270cc12c88d038083b47129cacd
Jeff Horton <jeff@goblinoid.co.uk>
2021-12-06 17:43:16 +0000

    styling baseline - Add sass and foundation - Setup fonts

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  sassOptions:     {
+    includePaths: [
+      path.join(__dirname, 'styles'),
+      path.join(__dirname, 'public'),
+      path.join(__dirname, 'node_modules', 'foundation-sites', 'scss'),
+    ],
+  },
 }
