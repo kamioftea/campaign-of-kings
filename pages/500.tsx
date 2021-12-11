9fe1ff355985e78abac4f6426d651d4aff8e160c
Jeff Horton <jeff@goblinoid.co.uk>
2021-12-11 17:32:55 +0000

    Basic 500 + tweak splash styles

---
+++ pages/500.tsx
@@ -0,0 +1,18 @@
+import {NextPage} from "next";
+import Link from "next/link";
+import styles from '../styles/SplashPage.module.scss'
+
+const Error500: NextPage = () => {
+    return <div className={styles.content}>
+        <h1><small>500</small> Something went wrong</h1>
+        <p>
+            Sorry there was an unexpected error processing your request. Please try again. If this keeps happening you
+            can email <a href="mailto:jeff@goblinoid.co.uk">jeff@goblinoid.co.uk</a> for support.
+        </p>
+        <Link href={"/"}>
+            <a>Return to Conquest of Hell's Claw Home</a>
+        </Link>
+    </div>
+}
+
+export default Error500
