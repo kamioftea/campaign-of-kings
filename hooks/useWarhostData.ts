76c8e4dd781ebb6650165dee12348e39f99ec3dc
Jeff Horton <jeff@goblinoid.co.uk>
2022-01-05 17:11:51 +0000

    Add model races to vanguard data

--- hooks/useWarhostData.ts
+++ hooks/useWarhostData.ts
@@ -2,12 +2,16 @@ import {useCallback, useEffect, useState} from "react";
 import {WarhostData} from "../model/WarhostData";
 import {WarhostUpdates} from "../model/Warhost";
 import {useUser} from "./use-user";
-import {UserDocument} from "../model/UserDocument";
+
+export interface ErrorMessage {
+    message: string,
+    renderPage: boolean,
+}
 
 export function useWarhostData(army_list: string | undefined) {
     const [isLoading, setIsLoading] = useState<boolean>(true);
     const [warhostData, setWarhostData] = useState<WarhostData | null>(null);
-    const [error, setError] = useState<string | null>(null);
+    const [error, setError] = useState<ErrorMessage | null>(null);
     const {setUser} = useUser();
 
     useEffect(() => {
@@ -28,9 +32,14 @@ export function useWarhostData(army_list: string | undefined) {
                 body: JSON.stringify(data)
             }
             )
-            .then(res => { if (res.status == 200) {return res.json();} else { throw new Error;} })
-            .then((json: UserDocument) => setUser(json))
-            .catch(err => setError(err.message))
+            .then(async res => {
+                if (res.status == 200) setUser(await res.json());
+                else if (res.status == 400) setError({message: await res.text(), renderPage: true});
+                else throw new Error()
+            })
+            .catch(() => {
+                setError({message: 'Failed to send the request to the server, please try again', renderPage: false});
+            })
     }, [])
 
     return {
