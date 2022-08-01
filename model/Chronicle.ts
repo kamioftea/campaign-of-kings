0e4fea7577a19936bf41e69eec9c94ea02f82bb2
Jeff Horton <jeff@goblinoid.co.uk>
2022-08-01 23:55:48 +0100

    Chronicle schema updates

--- model/Chronicle.ts
+++ model/Chronicle.ts
@@ -1,12 +1,26 @@
 import mongoose, {Schema} from "mongoose";
-import {ChronicleDocument} from "./ChronicleDocument";
+import {ChronicleContent, ChronicleDocument} from "./ChronicleDocument";
+import {ObjectID} from "bson";
 
-const chronicleSchema = new Schema<ChronicleDocument>({
+const chronicleContentSchema = new Schema<ChronicleContent>({
     title: String,
     slug: String,
     snippet: String,
-    body: [String],
+    body: String,
+    allowHtml: Boolean,
 });
+
+const chronicleSchema = new Schema<ChronicleDocument>({
+    chronicleType: String,
+    coverImageUrl: String,
+    authorId: ObjectID,
+    publishedDate: Date,
+    updatedDate: Date,
+    draftContent: chronicleContentSchema,
+    approvedContent: chronicleContentSchema,
+    reviewStatus: String,
+    reviewNotes: String,
+})
 
 export const Chronicle = mongoose.model<ChronicleDocument>("Chronicle", chronicleSchema);
 
--- model/ChronicleDocument.ts
+++ model/ChronicleDocument.ts
@@ -1,10 +1,33 @@
 import {Document} from "mongoose";
 
-export type ChronicleDocument = Document & {
+export enum ReviewStatus {
+    PENDING = "Pending",
+    ACCEPTED = "Accepted",
+    REJECTED = "Rejected",
+}
+
+export enum ChronicleType {
+    NARRATIVE,
+    BLOG,
+    MILESTONE
+}
+
+export type ChronicleContent = {
     title: string;
     slug: string;
-    published: Date;
-    draft: boolean;
     snippet: string;
     body: string;
+    allowHtml: boolean;
+}
+
+export type ChronicleDocument = Document & {
+    chronicleType: ChronicleType,
+    coverImageUrl?: string,
+    authorId?: string;
+    publishedDate?: Date;
+    updatedDate?: Date;
+    draftContent?: ChronicleContent;
+    approvedContent?: ChronicleContent;
+    reviewStatus?: ReviewStatus,
+    reviewNotes?: string,
 };
